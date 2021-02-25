d445ef0a9048f596e298c107f0d5bee84b93fae4
Marc de Mas <marcdmv@gmail.com>
2021-02-25 13:36:19 +0100

    cambio de tipo de proyecto

--- src/main/java/itacademy/Main.java
+++ src/main/java/itacademy/Main.java
@@ -1,11 +1,15 @@
-package mongodb;
+package itacademy;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
 
 public class Main {
 
     MongoClient mongoClient = MongoClients.create();
     MongoDatabase db = mongoClient.getDatabase("nueva");
+    MongoCollection <Document> coleccion = db.getCollection("profes");
 }
